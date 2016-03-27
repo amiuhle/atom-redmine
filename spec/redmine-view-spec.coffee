8b96376d7115110ea1b549a049733f033ea40fcb
Timo Uhlmann <t.uhlmann@zeroseven.de>
2016-03-27 23:54:49 +0200

    Converted specs to ES2015

--- spec/redmine-view-spec.coffee
+++
@@ -1,5 +0,0 @@
-RedmineView = require '../lib/redmine-view'
-
-describe "RedmineView", ->
-  it "has one valid test", ->
-    expect("life").toBe "easy"
---
+++ spec/views/issues-list-view-spec.js
@@ -0,0 +1,10 @@
+'use babel'
+/* eslint-env jasmine */
+
+// import IssuesListView from '../lib/views/issues-list-view'
+
+describe('RedmineView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy')
+  })
+})
